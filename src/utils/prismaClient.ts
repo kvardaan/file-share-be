9d6478d3bf4c2262abf8ac46d341b9879037b6da
kvardaan <vardaankhattar8907@gmail.com>
2025-07-17 22:00:36 +0530

    feat: added prisma orm

---
+++ src/utils/prismaClient.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client"
+
+import { config } from "./env"
+
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+type tPrismaClientSingleton = typeof prismaClientSingleton
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<tPrismaClientSingleton>
+} & typeof global
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export default prisma
+
+if (config.nodeEnv !== "production") {
+  globalThis.prismaGlobal = prisma
+}
