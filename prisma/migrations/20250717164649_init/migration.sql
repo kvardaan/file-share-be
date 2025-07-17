b88007531443a7b10c29dbaa984b4a5f2002493a
kvardaan <vardaankhattar8907@gmail.com>
2025-07-17 22:18:18 +0530

    feat: configured postgres

---
+++ prisma/migrations/20250717164649_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "File" (
+    "id" SERIAL NOT NULL,
+    "url" TEXT,
+    "metadata" JSONB,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "File_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "File_url_key" ON "File"("url");
